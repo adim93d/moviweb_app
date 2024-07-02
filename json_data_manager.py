2b8f6450ca3a9adb8c1255b0bc490304aebf3144
element <adim93d@gmail.com>
2024-07-02 23:42:53 +0300

    json data manager class setup completed

--- json_data_manager.py
+++ json_data_manager.py
@@ -6,10 +6,24 @@ class JSONDataManager(DataManagerInterface):
     def __init__(self, filename):
         self.filename = filename
 
+    def read_file(self):
+        with open(self.filename, 'r+') as handle:
+            data = json.load(handle)
+        return data
+
     def get_all_users(self):
         # Return all the users all users
-        pass
+        data = self.read_file()
+        users_dict = {}
+        for user_id, username in data.items():
+            users_dict[int(user_id)] = username['name']
+        return users_dict
 
     def get_user_movies(self, user_id):
         # Return all the movies for a given user
-        pass
+        data = self.read_file()
+        return data[str(user_id)]['movies']
+
+
+# JSONDataManager('temp_json_data.json').get_all_users()
+print(JSONDataManager('temp_json_data.json').get_user_movies(2))

efdfda58d7592f683d96dfb50302bdc948ba4cd0
element <adim93d@gmail.com>
2024-07-03 00:15:29 +0300

    added route map template for upcoming routhes

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
-pass
+from flask import Flask
+from datamanager.json_data_manager import JSONDataManager
+
+app = Flask(__name__)
+data_manager = JSONDataManager('data/movie_data.json')
+
+
+@app.route('/')
+def home():
+    return "Welcome to MovieWeb App!"
+
+
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return str(users)  # Temporarily returning
+
+
+@app.route('/users/<user_id>')
+def list_user_movies():
+    pass
+
+
+@app.route('/add_user')
+def add_user():
+    pass
+
+
+@app.route('/users/<user_id>/add_movie')
+def add_movie():
+    pass
+
+
+@app.route('/users/<user_id>/update_movie/<movie_id>')
+def update_movie():
+    pass
+
+
+@app.route('/users/<user_id>/delete_movie/<movie_id>')
+def delete_movie():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

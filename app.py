76e553144c34503514b3539f5398e910a3d8496b
element <adim93d@gmail.com>
2024-07-04 12:26:45 +0300

    Added function: list_user_movies

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datamanager.json_data_manager import JSONDataManager
 
 app = Flask(__name__)
 data_manager = JSONDataManager('data/movie_data.json')
+USERS = data_manager.get_all_users()
 
 
 @app.route('/')
@@ -12,13 +13,14 @@ def home():
 
 @app.route('/users')
 def list_users():
-    users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
+    return render_template('users.html', users=USERS)
 
 
 @app.route('/users/<user_id>')
-def list_user_movies():
-    pass
+def list_user_movies(user_id):
+    user_movies = data_manager.get_user_movies(user_id)
+    username = USERS.get(int(user_id))
+    return render_template('user_movies.html', movies=user_movies, username=username)
 
 
 @app.route('/add_user')

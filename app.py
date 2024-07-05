56aa134bbda86b380f6e04a2bb3e3b21047b0e5b
element <adim93d@gmail.com>
2024-07-05 14:37:00 +0300

    updated app.py to increase readability and better structure

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datamanager.json_data_manager import JSONDataManager
 import requests
-import json
 
 app = Flask(__name__)
 data_manager = JSONDataManager('data/movie_data.json')
@@ -11,6 +10,27 @@ API_KEY = 'apikey=3ac01df6'
 SEARCH_BY_TITLE = '&t='
 
 
+def get_users():
+    return data_manager.get_all_users()
+
+
+def get_user_movies(user_id):
+    return data_manager.get_user_movies(user_id)
+
+
+def read_data():
+    return data_manager.read_file()
+
+
+def write_data(data):
+    data_manager.write_file(data)
+
+
+def fetch_movie_details(movie_title):
+    response = requests.get(f"{REQUEST_URL}{API_KEY}{SEARCH_BY_TITLE}{movie_title}")
+    return response.json()
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -18,15 +38,16 @@ def home():
 
 @app.route('/users')
 def list_users():
-    users = data_manager.get_all_users()
+    users = get_users()
     return render_template('users.html', users=users)
 
 
 @app.route('/users/<user_id>')
 def list_user_movies(user_id):
-    user_movies = data_manager.get_user_movies(user_id)
+    user_movies = get_user_movies(user_id)
     username = USERS.get(int(user_id))
-    return render_template('user_movies.html', movies=user_movies, username=username, user_id=user_id)
+    message = request.args.get('message')
+    return render_template('user_movies.html', movies=user_movies, username=username, user_id=user_id, message=message)
 
 
 @app.route('/add_user', methods=['GET'])
@@ -37,10 +58,10 @@ def add_user_form():
 @app.route('/add_user', methods=['POST'])
 def add_user():
     username = request.form.get('name')
-    users = data_manager.read_file()
+    users = read_data()
     new_user_id = str(max(int(user_id) for user_id in users.keys()) + 1 if users else 1)
     users[new_user_id] = {"name": username, "movies": {}}
-    data_manager.write_file(users)
+    write_data(users)
     return redirect(url_for('list_users'))
 
 
@@ -52,20 +73,21 @@ def add_movie_form(user_id):
 @app.route('/users/<user_id>/add_movie', methods=['POST'])
 def add_movie(user_id):
     movie_title_search = request.form.get('movie_name')
-    search_result = requests.get(REQUEST_URL + API_KEY + SEARCH_BY_TITLE + movie_title_search).json()
+    search_result = fetch_movie_details(movie_title_search)
 
     if search_result.get('Response') == 'True':
-        movie_id = str(len(data_manager.get_user_movies(user_id)) + 1)
+        movie_id = str(len(get_user_movies(user_id)) + 1)
         movie_details = {
             "movie_name": search_result['Title'],
             "producer": search_result['Director'],
             "year": search_result['Year'],
             "rating": search_result['imdbRating'],
-            "img_url": search_result['Poster']}
+            "img_url": search_result['Poster']
+        }
 
-        users = data_manager.read_file()
+        users = read_data()
         users[user_id]['movies'][movie_id] = movie_details
-        data_manager.write_file(users)
+        write_data(users)
         message = 'Movie added successfully!'
     else:
         message = 'Movie not found!'
@@ -75,14 +97,14 @@ def add_movie(user_id):
 
 @app.route('/users/<user_id>/update_movie/<movie_id>', methods=['GET'])
 def update_movie_form(user_id, movie_id):
-    users = data_manager.read_file()
+    users = read_data()
     movie_details = users[user_id]['movies'][movie_id]
     return render_template('update_movie.html', user_id=user_id, movie_id=movie_id, movie=movie_details)
 
 
 @app.route('/users/<user_id>/update_movie/<movie_id>', methods=['POST'])
 def update_movie(user_id, movie_id):
-    users = data_manager.read_file()
+    users = read_data()
     movie_details = users[user_id]['movies'][movie_id]
 
     movie_details['movie_name'] = request.form.get('movie_name')
@@ -91,15 +113,15 @@ def update_movie(user_id, movie_id):
     movie_details['rating'] = request.form.get('rating')
     movie_details['img_url'] = request.form.get('img_url')
 
-    data_manager.write_file(users)
+    write_data(users)
     return redirect(url_for('list_user_movies', user_id=user_id))
 
 
 @app.route('/users/<user_id>/delete_movie/<movie_id>', methods=['POST'])
 def delete_movie(user_id, movie_id):
-    users = data_manager.read_file()
+    users = read_data()
     del users[user_id]['movies'][movie_id]
-    data_manager.write_file(users)
+    write_data(users)
     return redirect(url_for('list_user_movies', user_id=user_id))
 
 
